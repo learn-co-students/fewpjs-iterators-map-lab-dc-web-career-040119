96fb0caa318ff5b650e5dfacaacec389891998da
Matthew Lindsey <mbl@null.net>
2019-05-14 14:58:48 -0400

    Done.

--- index.js
+++ index.js
@@ -8,15 +8,16 @@ const tutorials = [
   'Immutable State and Pure Functions',
   'what is the difference between == and ===?',
   'what is the difference between event capturing and bubbling?',
-  'what is JSONP?'
+  'what is JSONP?',
 ];
 
-const titleCased = () => {
-  return tutorials.map(function(tutorial) {
+const titleCased = () =>
+  tutorials.map(tutorial => {
     let words = tutorial.split(' ');
-    let capitalizedWords = words.map(function(word) {
-      return word.charAt(0).toUpperCase() + word.substr(1);
-    });
+    let capitalizedWords = words.map(word =>
+      word.charAt(0).toUpperCase() + word.substr(1)
+    );
     return capitalizedWords.join(' ');
-  })
-}
+  });
+
+console.log(titleCased());
