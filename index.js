99bc2f7e464b37f1e08ef4701bc9cd87e1567fd9
Cristina Cristobal <cristina.cristobal@gmail.com>
2019-05-14 11:32:18 -0400

    Done.

--- index.js
+++ index.js
@@ -11,16 +11,20 @@ const tutorials = [
   'what is JSONP?'
 ];
 
-// const titleCased = () => {
-//   return tutorials
-// }
-
-function titleCased() {
-  return tutorials.map(function (tutorial) {
-    tutorial = tutorial.split(' ');
+// turn all of the strings in the above array into titleCase
+function titleCased(){
+// return the result of a .map transformation of the tutorials array
+  return tutorials.map(tutorial => {
+    // separate every word in each string by a comma
+    tutorial = tutorial.split(' ')
+    //iterate through every word. find the character of the word at index zero and
+    // make it uppercase, then add it back to the rest of letters
     for (var i = 0; i < tutorial.length; i++) {
-      tutorial[i] = tutorial[i].charAt(0).toUpperCase() + tutorial[i].slice(1);
+
+      tutorial[i] = tutorial[i].charAt(0).toUpperCase() + tutorial[i].slice(1)
     }
-    return tutorial.join(' ');
-  });
+    // after that work has been done, .join everything back to their original strings
+    // with a comma in between each course name
+    return tutorial.join(' ')
+  })
 }
